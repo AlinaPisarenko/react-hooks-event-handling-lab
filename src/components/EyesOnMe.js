1b6015acb1170395646c0e8c6ddad1a254351957
Alina <pisarenko.alina@gmail.com>
2022-04-26 15:57:33 -0400

    commit tree bc7cbd82b4a6b01c71843273cb6dcec8ab6ed6be

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
-// Code EyesOnMe Component Here
+function EyesOnMe() {
+  function blur() {
+    return console.log("Hey! Eyes on me!");
+  }
+  return (
+    <div>
+      <button onBlur={blur} onFocus={() => console.log("Good!")}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
